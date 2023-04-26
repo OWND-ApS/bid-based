08201ac57763ef1d0fe6e06ff84787f6dd37b778
Anker Bach Ryhl <ankerbachryhl@gmail.com>
2023-04-26 10:14:35 +0200

    created swap in tests

--- test/BidProtocol.t.sol
+++ test/BidProtocol.t.sol
@@ -42,9 +42,8 @@ contract BidTest is Test {
         assertEq(newState, 1);
     }
 
-    function test_swapIn(uint256 a, uint256 b, uint256 c) public {
+    function test_swapInPercent(uint256 a, uint256 b, uint256 c) public {
         //Can't be more than 1% of bid price which is 10 ether
-
         if (a <= 0 || b <= 0 || c <= 0) {
             return;
         }
@@ -102,9 +101,55 @@ contract BidTest is Test {
 
         uint256 percentNow = bidPool.percentInPool();
         assertEq(currentPercent - (cPercent + bPercent + aPercent), percentNow);
+    }
+
+    function test_swapInPoolSize(uint256 a, uint256 b, uint256 c) public {
+        //Can't be more than 1% of bid price which is 10 ether
+        if (a <= 0 || b <= 0 || c <= 0) {
+            return;
+        }
+        if (a > 1e17 || b > 1e17 || c > 1e17) {
+            return;
+        }
+
+        BidProtocol bidPool = new BidProtocol(
+            address(0),
+            0,
+            address(0),
+            0,
+            4 ether
+        );
+        bidPool.init{value: 1 ether}();
+
+        ReservoirOracle.Message memory message = ReservoirOracle.Message(
+            0xf0e3c933e571a6ed56b569838684309bb69ba3a039372a3d15a3dd0fe715fef5,
+            "0x00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001354ee1e9c1e8000",
+            1682448083,
+            "0x4ddc00f663cf965476ecba368361506ecf4cac7f5622d614c53115f64e2bf2370425b8958044493c7410e624592dcd3fbf78e36caf1feb97d7b2b6a38efdcb411c"
+        );
+
+        uint256 currentPool = bidPool.poolSize();
+
+        address aUser = vm.addr(1);
+        vm.prank(aUser);
+        vm.deal(aUser, 1 ether);
+
+        bidPool.swapIn{value: a}(message);
+
+        address bUser = vm.addr(2);
+        vm.prank(bUser);
+        vm.deal(bUser, 1 ether);
+
+        bidPool.swapIn{value: b}(message);
+
+        address cUser = vm.addr(3);
+        vm.prank(cUser);
+        vm.deal(cUser, 1 ether);
+
+        bidPool.swapIn{value: c}(message);
 
         uint256 poolNow = bidPool.poolSize();
-        require(poolNow > currentPool);
+        assertEq(poolNow - a - b - c, currentPool);
     }
 
     //Not testing right now since private
